32ba1d7421afccc7bcc20ffc646663554cc72cf0
alesiosaba <alesiosaba@gmail.com>
2021-07-14 13:08:31 -0300

    parciales 2021

---
+++ PARCIAL 2021/tema B/Saba Lagos, Leonardo - 1erParcial 20210714b.sql
@@ -0,0 +1,208 @@
+﻿/*2.Query
+Mostrar 
+	el código y descripción del Estado,
+	código y descripción del Tipo de Producto 
+	y la cantidad de unidades vendidas 
+	
+	de aquellos tipos de productos más vendidos (por cantidad) en cada Estado. 
+
+Si hubiese más de un tipo de producto en un mismo estado 
+con la misma cantidad de unidades vendidas máxima, 
+informarlos a todos.
+
+Mostrar el resultado ordenado por cantidad total vendida en orden descendente y,
+en caso que las cantidades de varios productos sean iguales,
+por la Descripción del Estado en forma ascendente, 
+si los productos con cantidades iguales son del mismo estado, 
+ordenar además por descripción del tipo de producto en forma ascendente.
+*/
+
+SELECT 
+	j.Cod_Estado AS Cod_Estado,
+	j.Descripcion_Estado AS Descripcion_Estado,
+	stock_num AS Cod_Tipo_Producto,
+	description AS Descripcion_Tipo_Producto,
+	j.Cant_Total_Vendida AS Cant_Total_Vendida
+FROM product_types pto
+RIGHT JOIN 
+(
+	SELECT 
+		st.state AS Cod_Estado,
+		st.sname AS Descripcion_Estado,
+		pt.stock_num AS Cod_Tipo_Producto,
+		pt.description AS Descripcion_Tipo_Producto,
+		SUM(i.quantity) AS Cant_Total_Vendida
+	FROM items i 
+		JOIN product_types pt ON pt.stock_num = i.stock_num
+		JOIN orders o ON o.order_num = i.order_num
+		JOIN customer c ON c.customer_num = o.customer_num  
+		JOIN state st ON st.state = c.state
+	GROUP BY st.state, st.sname, pt.stock_num, pt.description
+	HAVING SUM(i.quantity) =   
+	(SELECT TOP 1 SUM(i.quantity)
+	 FROM items i
+		JOIN orders o ON o.order_num = i.order_num
+		JOIN customer c ON c.customer_num = o.customer_num  
+		JOIN state st ON st.state = c.state
+	GROUP BY st.state, i.stock_num
+	ORDER BY 1 DESC)
+) j ON j.Cod_Tipo_Producto = pto.stock_num AND j.Descripcion_Tipo_Producto = pto.description
+
+ORDER BY j.Cant_Total_Vendida, j.Descripcion_Estado, pto.description
+
+------------------------------------------------------------------
+
+
+/*3.Store Procedure
+Desarrollar un stored procedure que realice la inserción o modificación de un producto determinado.
+
+Parámetros de Entrada STOCK_NUM, MANU_CODE, UNIT_PRICE, UNIT_CODE, DESCRIPTION
+Previamente a realizar alguna operación validar:
+EXISTENCIA de MANU_CODE en Tabla MANUFACT - Informando Error por Fabricante Inexistente.
+EXISTENCIA del atributo  UNIT_CODE en la Tabla UNITS - Informando Error por Código de Unidad Inexistente
+EXISTENCIA en Tabla PRODUCT_TYPES – 
+	Si no existe INSERTAR Registro en la tabla PRODUCT_TYPES
+	Sino realizar el UPDATE del atributo ‘description’.
+
+ Una vez validados los parámetros: 
+ Si el producto no existe, insertarlo. 
+ En caso que ya exista, actualizar los atributos no clave.
+ */
+
+GO
+CREATE PROCEDURE punto3 @stock_num SMALLINT, @manu_code CHAR(3), @unit_price DECIMAL, @unit_code SMALLINT, @description VARCHAR(15)
+AS
+BEGIN 
+	BEGIN TRY
+		BEGIN TRANSACTION
+			-- Previamente a realizar alguna operación validar:
+			
+			-- EXISTENCIA de MANU_CODE en Tabla MANUFACT - Informando Error por Fabricante Inexistente.
+			IF NOT EXISTS(SELECT 1 FROM manufact WHERE manu_code = @manu_code)
+				THROW 50000, 'Fabricante Inexistente' , 1
+
+			-- EXISTENCIA del atributo  UNIT_CODE en la Tabla UNITS - Informando Error por Código de Unidad Inexistente
+			IF NOT EXISTS(SELECT 1 FROM units WHERE unit_code = @unit_code)
+				THROW 50001, 'Fabricante Inexistente' , 1
+			
+			-- EXISTENCIA en Tabla PRODUCT_TYPES – 
+				-- Si no existe INSERTAR Registro en la tabla PRODUCT_TYPES
+				-- Sino realizar el UPDATE del atributo ‘description’.
+			IF NOT EXISTS(SELECT 1 FROM product_types WHERE stock_num = @stock_num)
+				INSERT INTO product_types(stock_num, description)
+				VALUES(@stock_num, @description)
+			ELSE
+				UPDATE product_types
+					SET description = @description
+					WHERE stock_num = @stock_num
+
+			-- Una vez validados los parámetros: 
+				-- Si el producto no existe, insertarlo. 
+				-- En caso que ya exista, actualizar los atributos no clave.
+
+			IF NOT EXISTS(SELECT 1 FROM products WHERE stock_num = @stock_num AND manu_code = @manu_code)
+				INSERT INTO products(stock_num, manu_code, unit_price, unit_code)
+				VALUES(@stock_num, @manu_code, @unit_price, @unit_code)
+			ELSE 
+				UPDATE products
+					SET unit_price = @unit_price, unit_code = @unit_code
+					WHERE stock_num = @stock_num AND manu_code = @manu_code
+
+		COMMIT TRANSACTION
+	END TRY
+
+	BEGIN CATCH
+		ROLLBACK TRANSACTION
+	END CATCH
+END
+
+
+/*
+4.Trigger
+Dada la vista:
+      CREATE VIEW OrdenItems AS 
+      SELECT o.order_num, o.order_date, o.customer_num, o.paid_date, 
+   i.item_num, i.stock_num, i.manu_code, i.quantity, i.unit_price 
+        FROM orders o JOIN items i ON o.order_num = i.order_num;
+
+Se desea manejar las operaciones de ALTA sobre la vista anterior.
+
+Los controles a realizar son los siguientes:
+	a.No se permitirá que una Orden contenga ítems de fabricantes de más de dos estados en la misma orden.
+	b.Por otro parte los Clientes del estado de ALASKA no podrán realizar compras a fabricantes fuera de ALASKA.
+Notas:
+	 Las altas son de una misma Orden y de un mismo Cliente pero pueden venir varias líneas de ítems en una ORDEN.
+	 Ante el incumplimiento de una validación, deshacer TODA la transacción y finalizar la ejecución.
+*/
+
+GO
+CREATE VIEW OrdenItems AS 
+	SELECT 
+		o.order_num,
+		o.order_date,
+		o.customer_num,
+		o.paid_date, 
+		i.item_num,
+		i.stock_num,
+		i.manu_code,
+		i.quantity,
+		i.unit_price 
+FROM orders o
+	JOIN items i ON o.order_num = i.order_num
+
+GO
+CREATE TRIGGER insert_OrdenItems ON OrdenItems
+INSTEAD OF INSERT AS
+BEGIN
+
+	BEGIN TRY 
+		
+		BEGIN TRANSACTION
+
+		DECLARE @order_num SMALLINT, @order_date DATETIME, @customer_num SMALLINT, @paid_date DATETIME, @item_num SMALLINT, @stock_num SMALLINT, @manu_code CHAR(3), @quantity SMALLINT, @unit_price DECIMAL
+
+		DECLARE cursor_inserted CURSOR FOR
+		SELECT order_num, order_date, customer_num, paid_date, item_num, stock_num, manu_code, quantity, unit_price 
+		FROM inserted
+
+		OPEN cursor_inserted
+
+		FETCH NEXT FROM cursor_inserted
+		INTO @order_num, @order_date, @customer_num, @paid_date, @item_num, @stock_num, @manu_code, @quantity, @unit_price
+
+		WHILE @@FETCH_STATUS = 0
+		BEGIN 
+
+			-- Los controles a realizar son los siguientes:
+
+			-- a.No se permitirá que una Orden contenga ítems de fabricantes de más de dos estados en la misma orden.
+
+
+			IF( (SELECT COUNT(DISTINCT m.state) FROM items i JOIN manufact m ON m.manu_code = i.manu_code WHERE order_num = @order_num) > 2)
+				THROW 50000, 'Una orden no puede contener ítems de fabricantes de más de dos estados en la misma orden' , 1
+			
+			-- b.Por otro parte los Clientes del estado de ALASKA no podrán realizar compras a fabricantes fuera de ALASKA.
+			IF ( (SELECT state FROM customer WHERE customer_num = @customer_num) = 'AK' AND EXISTS( SELECT 1 FROM manufact WHERE manu_code = @manu_code AND state != 'AK') )
+				THROW 50000, 'Los Clientes del estado de ALASKA no podrán realizar compras a fabricantes fuera de ALASKA' , 1
+
+			INSERT INTO OrdenItems(order_num, order_date, customer_num, paid_date, item_num, stock_num, manu_code, quantity, unit_price)
+			VALUES(@order_num, @order_date, @customer_num, @paid_date, @item_num, @stock_num, @manu_code, @quantity, @unit_price)
+
+
+			FETCH NEXT FROM cursor_inserted
+			INTO @order_num, @order_date, @customer_num, @paid_date, @item_num, @stock_num, @manu_code, @quantity, @unit_price
+
+		END
+
+		COMMIT TRANSACTION
+	END TRY
+
+	BEGIN CATCH
+		ROLLBACK TRANSACTION
+	END CATCH
+
+	CLOSE cursor_inserted
+	DEALLOCATE cursor_inserted
+	
+	
+END
